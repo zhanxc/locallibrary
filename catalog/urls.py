0c8673cff8daed1fa911e1f97d47bbb991621c21
zhanxc <1974346076@qq.com>
2022-11-26 18:37:40 +0800

    添加书本详细页面

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,8 +3,8 @@ from catalog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('books/', views.books, name='books'),
-    # path('books/', views.BookListView.as_view(), name='books'),
+    # path('books/', views.books, name='books'),
+    path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.views import generic
 from .models import Book, Author, BookInstance, Genre
@@ -21,39 +22,42 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+# 返回图书列表
+# def books(request):
+#     book_list = Book.objects.all().get()
 
-def books(request):
-    book_list = Book.objects.all().get()
+#     return render(
+#         request,
+#         book_list.html,
+#         context=(
+#             book_list
+#         )
+#     )
 
-    return render(
-        request,
-        book_list.html,
-        context=(
-            book_list
-        )
-    )
-
-# class BookListView(generic.ListView):
-    # model = Book
+class BookListView(generic.ListView):
+    model = Book
     # context_object_name = 'book_list'   # your own name for the list as a template variable
     # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
-    # template_name = 'templates/book_list.html'  # Specify your own template name/location
+    template_name = 'book_list.html'  # Specify your own template name/location
 
 class BookDetailView(generic.DetailView):
     model = Book
 
-    def book_detail_view(request,pk):
-        try:
-            book_id=Book.objects.get(pk=pk)
-        except Book.DoesNotExist:
-            raise Http404("Book does not exist")
+    template_name = 'book_detail.html'  # Specify your own template name/location
 
-        #book_id=get_object_or_404(Book, pk=pk)
+    # 如果记录不存，抛出404异常，同时返回book id
+    # def book_detail_view(request,pk):
+    #     try:
+    #         book_id=Book.objects.get(pk=pk)
+    #     except Book.DoesNotExist:
+    #         raise Http404("Book does not exist")
 
-        return render(
-            request,
-            'catalog/book_detail.html',
-            context={'book':book_id,}
-        )
+    #     #book_id=get_object_or_404(Book, pk=pk)
+
+    #     return render(
+    #         request,
+    #         'catalog/book_detail.html',
+    #         context={'book':book_id,}
+    #     )
 
 
