c858e0131723257bea4d40a0c2d85019e2b5280c
zhanxc <1974346076@qq.com>
2022-12-06 15:42:13 +0800

    添加用户认证模块代码

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # locallibrary
 offline book manage site
-
+<https://developer.mozilla.org/zh-CN/docs/Learn/Server-side/Django/Authentication>
 # features
-1. 
-2. 
-3. 
+1. 书本管理
+2. 用户管理
+3. 借阅管理
 
 # index page
 <http://127.0.0.1:8000/index>
@@ -13,6 +13,7 @@ offline book manage site
 <http://127.0.0.1:8000/admin>
 
 # users
-1. andy/locallibray01
+1. andy/9o0p(O)P
+2. 
 
 
--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/catalog/'
+
+# 控制台显示重置密码链接
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
